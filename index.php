682129885a9dcd87bb1a74e8c09d02f25cd0e5ef
elbren <elbrenantonio@sksu.edu.ph>
2026-03-11 11:22:49 +0800

    test update to aws

--- index.php
+++ index.php
@@ -553,7 +553,7 @@ if (isset($_POST['login'])) {
 
           <div class="card login-card">
             <div class="card-body">
-              <div class="login-brand">sksu synk</div>
+              <div class="login-brand">sksu synk sksu</div>
 
               <h1 class="login-title">Master Scheduling Workspace</h1>
               <p class="login-subtitle">
